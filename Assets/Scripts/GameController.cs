20ae960858ccfcc49d7c350061c1f30f305edfb7
EforEveryOne <31865908+EforEveryOne@users.noreply.github.com>
2017-11-25 02:01:05 -0500

    Please work this time.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+
+	int gridX = 8;
+	int gridY = 5;
+	float turnTime = 2;
+	int turnCounter = 0;
+
+
+
+	int gameLength;
+	static GameObject[,] grid;
+	static Color[] myColors; 
+	public int x, y;
+
+
+	void CreateGrid () {
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		CreateGrid ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > turnTime * turnCounter) {
+			turnCounter++;
+		}	
+	}
+		
+	}
