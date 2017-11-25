28b15ac049bb0a9689d08580b6e48b676df84142
EforEveryOne <31865908+EforEveryOne@users.noreply.github.com>
2017-11-25 15:10:14 -0500

    Finding whitecubes again.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,6 +14,15 @@ public class GameController : MonoBehaviour {
 	float turnTime = 2;
 	int turnCounter = 0;
 	Color[] myColors = { Color.blue, Color.red, Color.green, Color.yellow, Color.magenta };
+	int score = 0;
+
+
+	// Use this for initialization
+	void Start () {
+
+		CreateGrid ();
+
+	}
 
 	void CreateGrid () {
 		grid = new GameObject[gridX, gridY];
@@ -21,7 +30,9 @@ public class GameController : MonoBehaviour {
 		for (int y = 0; y < gridY; y++) {
 			for (int x = 0; x < gridX; x++) {
 				cubePos = new Vector3 (x * 2, y * 2, 0);
-		        Instantiate (cubePrefab, cubePos, Quaternion.identity);
+				grid[x,y] = Instantiate (cubePrefab, cubePos, Quaternion.identity);
+				grid [x, y].GetComponent<CubeController> ().myX = x;
+				grid [x, y].GetComponent<CubeController> ().myY = y;
 			}
 		}
 	}
@@ -35,23 +46,145 @@ public class GameController : MonoBehaviour {
 		nextCube.GetComponent<Renderer> ().material.color = myColors [Random.Range (0, myColors.Length)];
 	}
 
-	// Use this for initialization
-	void Start () {
-
-		CreateGrid ();
-		
+	// Ends the game, called when end game conditions are met.
+	void EndGame (bool win) {
+		if (win) {
+			print ("You win!");
+		}
+		else {
+			print ("You lose. Please try again!");
+		}
 	}
+
+
+
+
+
+	GameObject PickWhiteCube (List<GameObject> whiteCubes) {
+		if (whiteCubes.Count == 0) {
+			// error value
+			return null;
+		}
+
+		// pick a random white cube
+		return whiteCubes [Random.Range(0, whiteCubes.Count)];
+	}
+		
+	GameObject FindAvailableCube (int y) {
+		List<GameObject> whiteCubes = new List<GameObject> ();
+		for (int x = 0; x < gridX; x++) {
+			if (grid [x, y].GetComponent<Renderer> ().material.color == Color.white) {
+				whiteCubes.Add (grid[x, y]);
+			}
+		}
+		return PickWhiteCube (whiteCubes);
+	}
+	GameObject FindAvailableCube () {
+		List<GameObject> whiteCubes = new List<GameObject> ();
+		for (int y = 0; y < gridY; y++) {
+			for (int x = 0; x < gridX; x++) {
+				if (grid [x, y].GetComponent<Renderer> ().material.color == Color.white) {
+					whiteCubes.Add (grid[x, y]);
+				}
+			}
+		}
+		return PickWhiteCube (whiteCubes);
+	}
+		
+
+	//set cube color or end game
+	void SetCubeColor (GameObject myCube, Color color) {
+		//no available cube in that row
+		if (myCube == null) {
+			EndGame (false);
+		} else {
+			//gives the cube in row the color of the next cube
+			myCube.GetComponent<Renderer> ().material.color = color;
+			Destroy (nextCube);
+			nextCube = null;
+		}	
+	}
+
+
+
+	void PlaceNextCube (int y) {
+		GameObject whiteCube = FindAvailableCube (y);
+		SetCubeColor (whiteCube, nextCube.GetComponent<Renderer> ().material.color);
+	}
+
+	void AddBlackCube() {
+		GameObject whiteCube = FindAvailableCube ();
+		SetCubeColor (whiteCube, nextCube.GetComponent<Renderer> ().material.color);
+
+		if (whiteCube == null) {
+			EndGame (false);
+		}
+		else {
+			// set the chosen cube to black
+			whiteCube.GetComponent<Renderer> ().material.color = Color.black;
+		}
+		//SetCubeColor (whiteCube, Color.black);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	void ProcessKeyboardInput () {
+		int numKeyPressed = 0;
+
+		if (Input.GetKeyDown (KeyCode.Alpha1) || Input.GetKeyDown (KeyCode.Keypad1)) {
+			numKeyPressed = 5;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2) || Input.GetKeyDown (KeyCode.Keypad2)) {
+			numKeyPressed = 4;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3) || Input.GetKeyDown (KeyCode.Keypad3)) {
+			numKeyPressed = 3;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha4) || Input.GetKeyDown (KeyCode.Keypad4)) {
+			numKeyPressed = 2;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha5) || Input.GetKeyDown (KeyCode.Keypad5)) {
+			numKeyPressed = 1;
+		}
+		// if there is a cube in the nextcube area
+		if (nextCube != null && numKeyPressed != 0) {
+
+			// place it in the designated row,
+			//-1 because grid array is 0 base, this way makes more sense to align with x/y value
+			PlaceNextCube (numKeyPressed-1);
+		}
+	}
+
+
+
+
+
+
 	
 	// Update is called once per frame
 	void Update () {
-		
+		ProcessKeyboardInput ();
+
 		if (Time.time > turnTime * turnCounter) {
 			turnCounter++;
 
-
+			// if we still have an exisiting next cube when the turn passes
+			if (nextCube != null) {
+				score -= 1;
+				AddBlackCube ();
+			//	FailToPlaceNextCube ();
+			}
 			CreateNextCube ();
-
 		}	
 	}
-		
 	}
